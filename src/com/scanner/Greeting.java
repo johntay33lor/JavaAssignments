bba4a4641f462c18c5d5e1465a0370fe8ea61e7d
John <johntay33lor@gmail.com>
2023-05-09 12:38:39 -0400

    Scanner

---
+++ src/com/scanner/Greeting.java
@@ -0,0 +1,16 @@
+package com.scanner;
+import java.util.Scanner;
+
+public class Greeting {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("What's your name?");
+
+        String name = input.nextLine();
+
+        System.out.println("Hi: " + name);
+
+    }
+
+}
