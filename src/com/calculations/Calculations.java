d24623ac33eaf522acf7aec12ffda2813a84dbb9
John <johntay33lor@gmail.com>
2023-05-09 15:25:55 -0400

    javaAssignment

---
+++ src/com/calculations/Calculations.java
@@ -0,0 +1,30 @@
+package com.calculations;
+import java.util.Scanner;
+public class Calculations {
+    public static void main(String[] args) {
+
+        int num1;
+        int num2;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter a number:");
+        num1 = input.nextInt();
+
+        System.out.print("Enter another number:");
+        num2 = input.nextInt();
+
+        System.out.println(num1 + " + " + num2 + "= " + (num1 + num2));
+        System.out.println(num1 + " - " + num2 + "= " + (num1 - num2));
+        System.out.println(num1 + " * " + num2 + "= " + (num1 * num2));
+        System.out.println(num1 + " / " + num2 + "= " +  num1 / num2);
+        System.out.println(num1 + " % " + num2 + "= " + (num1 % num2));
+
+
+
+
+
+
+
+    }
+}
--- src/com/scanner/VariableInputs.java
+++ src/com/scanner/VariableInputs.java
@@ -2,18 +2,31 @@ package com.scanner;
 import java.util.Scanner;
 public class VariableInputs {
     public static void main(String[] args) {
+
+        String string;
+        int num1;
+        double num2;
+        boolean answer;
+
         Scanner input = new Scanner(System.in);
 
         System.out.println("Enter a string of text:");
-        input.nextLine();
+        string = input.nextLine();
 
         System.out.print("Enter an integer:");
-        input.nextInt();
+        num1 = input.nextInt();
 
         System.out.print("Enter a double:");
-        input.nextDouble();
+        num2 = input.nextDouble();
 
         System.out.print("Enter a boolean:");
-        input.nextBoolean();
+        answer = input.nextBoolean();
+
+        System.out.println("Your string is " + string);
+        System.out.println("Your integer is " + num1);
+        System.out.println("Your double is " + num2);
+        System.out.println("Your boolean is " + answer);
+
+
     }
 }
